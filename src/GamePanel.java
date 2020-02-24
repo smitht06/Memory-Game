4de8482e69cdca3d65f374d35f72ab547d03d2cf
Anthony Smith <as361@students.uwf.edu>
2020-02-23 20:49:29 -0500

    added image paths and action listener for buttons

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,9 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Random;
 
 public class GamePanel extends JPanel{
-    private String[] imagePaths;
+    private static String[] imagePaths = {"images/bulbasaur.png","images/butterfree.png","images/dragonite.png","images/Eevee.png",
+            "images/Meowth.png","images/nidoran.png","images/pikachu.png","images/wartortle.png"} ;
     private int numberOfButtons;
     private JButton[] buttonsArray;
     private ImageIcon imageOnBack;
@@ -12,8 +15,10 @@ public class GamePanel extends JPanel{
     private int score = 0;
     private int index = 0;
     private int oddClick = 0;
+    private int numberOfClicks = 0;
     private Timer timer;
     private int numFlippedCards;
+    private boolean gameOver = false;
 
     //construct the gameboard that will hold the buttons and pictures
     public GamePanel(){
@@ -21,7 +26,7 @@ public class GamePanel extends JPanel{
         imageOnBack = new ImageIcon(this.getClass().getResource("images/back-of-card.png"));
         setBackground(Color.BLUE);
         setVisible(true);
-        addButtons();
+         addButtons();
     }
 
     public void addButtons(){
@@ -41,7 +46,7 @@ public class GamePanel extends JPanel{
             imageOnFront[i] = imageOnFront[j];
             imageOnFront[j] = temp;
         }
-        
+
     }
     private int makeButtons(int j){
         buttonsArray[j] = new JButton();
@@ -51,4 +56,140 @@ public class GamePanel extends JPanel{
         return j;
     }
 
+    private class ButtonListener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            if(timer.isRunning()){
+                return;
+            }numFlippedCards++;
+            for(int i = 0; i < numberOfButtons; i++){
+                if(e.getSource()==buttonsArray[i]){
+                    buttonsArray[i].setIcon(imageOnFront[i]);
+                    index = i;
+                }
+            }
+            if(numFlippedCards % 2 == 0){
+                if(index == oddClick){
+                    numberOfClicks--;
+                    return;
+                }
+                if(imageOnFront[index] != imageOnFront[oddClick]){
+                    timer.start();
+                }else{
+                    buttonsArray[index].setEnabled(false);
+                    buttonsArray[oddClick].setEnabled(false);
+                    score++;
+                    System.out.println("Match found!" + score);
+                    if(score == 8){
+                        JOptionPane.showMessageDialog(null,"You win!");
+                        setGameOver(true);
+                    }
+                }
+            }
+        }
+    }
+
+
+    public boolean isGameOver() {
+        return gameOver;
+    }
+
+    public void setGameOver(boolean gameOver) {
+        this.gameOver = gameOver;
+    }
+
+    public String[] getImagePaths() {
+        return imagePaths;
+    }
+
+    public void setImagePaths(String[] imagePaths) {
+        this.imagePaths = imagePaths;
+    }
+
+    public int getNumberOfButtons() {
+        return numberOfButtons;
+    }
+
+    public void setNumberOfButtons(int numberOfButtons) {
+        this.numberOfButtons = numberOfButtons;
+    }
+
+    public JButton[] getButtonsArray() {
+        return buttonsArray;
+    }
+
+    public void setButtonsArray(JButton[] buttonsArray) {
+        this.buttonsArray = buttonsArray;
+    }
+
+    public ImageIcon getImageOnBack() {
+        return imageOnBack;
+    }
+
+    public void setImageOnBack(ImageIcon imageOnBack) {
+        this.imageOnBack = imageOnBack;
+    }
+
+    public ImageIcon[] getImageOnFront() {
+        return imageOnFront;
+    }
+
+    public void setImageOnFront(ImageIcon[] imageOnFront) {
+        this.imageOnFront = imageOnFront;
+    }
+
+    public ImageIcon getTemp() {
+        return temp;
+    }
+
+    public void setTemp(ImageIcon temp) {
+        this.temp = temp;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public int getOddClick() {
+        return oddClick;
+    }
+
+    public void setOddClick(int oddClick) {
+        this.oddClick = oddClick;
+    }
+
+    public int getNumberOfClicks() {
+        return numberOfClicks;
+    }
+
+    public void setNumberOfClicks(int numberOfClicks) {
+        this.numberOfClicks = numberOfClicks;
+    }
+
+    public Timer getTimer() {
+        return timer;
+    }
+
+    public void setTimer(Timer timer) {
+        this.timer = timer;
+    }
+
+    public int getNumFlippedCards() {
+        return numFlippedCards;
+    }
+
+    public void setNumFlippedCards(int numFlippedCards) {
+        this.numFlippedCards = numFlippedCards;
+    }
 }
