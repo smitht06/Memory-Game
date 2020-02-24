ad8d45c322d78a3c401ad7a2a6a2fa5dca640e8d
Anthony Smith <as361@students.uwf.edu>
2020-02-23 19:40:41 -0500

    added panels fields

---
+++ src/GamePanel.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class GamePanel {
+    private String[] imagPaths;
+    private int numButtons;
+    private JButton[] buttonsArray;
+    private ImageIcon imageOnBack;
+    private ImageIcon[] imageOnFront;
+    private ImageIcon temp;
+    private int score = 0;
+    private int index = 0;
+    private int oddClick = 0;
+    private Timer timer;
+    private int numFlippedCards;
+    
+}
--- src/GameWindow.java
+++ src/GameWindow.java
@@ -2,13 +2,13 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GameWindow extends JFrame {
+    //JFrame constructor
     public GameWindow(){
+        //set title, visable, size exit on close and layout
         setTitle("Memory Game");
-        setBounds(5,5,900,700);
         setVisible(true);
         setSize(800,800);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new GridLayout(3,7));
-
     }
 }
